2bb3d3325b554437ab2aae39b9904905ad178af4
unknown <a01705777@itesm.mx>
2021-02-14 23:32:05 -0600

    Lab3 Finished

---
+++ lab5/online_store.js
@@ -0,0 +1,10 @@
+if(document.getElementById("pay").clicked == true) {
+    document.write("Hola"); 
+}
+else {
+    document.write("no"); 
+}
+
+var quant1 = document.getElementById("q3").innerHTML;
+document.write(quant1).innerHTML; 
+
