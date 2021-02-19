9175881509fb6b465bc2c0f8f60a99eb0051722b
RafaelHinojosa <a01705777@itesm.mx>
2021-02-19 13:05:02 -0600

    357Code done, falta probarlo

---
+++ lab8/357UVa.js
@@ -0,0 +1,58 @@
+/*
+Autor: Rafael Hinojosa López
+Matrícula: A01705777
+Problema: 357 - Let Me Count The Ways
+Juez: UVa Online Judge
+Descripción Original: https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&category=5&page=show_problem&problem=293
+U-debug: https://www.udebug.com/UVa/357
+*/
+
+function initMat_acm() {
+    dp[0] = [];
+    for(let i=0; i<=30000; i++) {
+        dp[0][i] = 1; 
+    }
+
+    for(let i=1; i<5; i++) {
+        dp[i] = [];
+        for(let j=0; j<=30000; j++) {
+            dp[i][j] = dp[i-1][j];
+            if(j - coins[i] >= 0) {
+                dp[i][j] += dp[i][j - coins[i]];
+            }
+        }
+    }
+
+    return; 
+}
+
+function solve_acm() { 
+    while(true) {
+        let n = prompt("Ingrese un número o presione Cancelar si desea ver el resultado: ");
+        if(n != null) {
+            let answer = dp[4][n]; 
+            if(answer == 1) {
+                document.write("There is only 1 way ");
+            }
+            else {
+                document.write("There are " + answer + " ways ");
+            }
+            document.write("to produce " + n + " cents change.")
+            document.write("<br>");
+        }
+        else {
+            break; 
+        }
+    }
+
+    return; 
+}
+
+var dp = Array(); 
+const coins = Array.of(1, 5, 10, 25, 50);
+initMat_acm(); 
+solve_acm();        
+
+
+
+ 
---
+++ lab8/web_app_html.js
@@ -0,0 +1,10 @@
+@canal Una aclaración con respecto al 
+laboratorio 8. Las páginas que generen, 
+todavía no serán capaces de obtener el 
+archivo CSS del servidor 
+(todavía no hemos visto cómo programarlo, 
+    lo vamos a ver la próxima semana). 
+Por lo que el servidor sólo regresará una 
+página sin estilos. Todo el código que se 
+genere debe ejecutarse en el servidor y 
+mostrar los resultados en el html.
