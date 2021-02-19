ec86e8f41b5314a412698812e75f2554c6d6a6cb
RafaelHinojosa <a01705777@itesm.mx>
2021-02-19 12:01:04 -0600

    writeInFile.js complete

--- lab8/writeInFile.js
+++ lab8/writeInFile.js
@@ -2,4 +2,18 @@
 function writeInFile(cadena) {
     const filesystem = require('fs'); 
     filesystem.writeFileSync('output.txt', cadena);     
+    console.log("La palabra es: " + cadena);
+
+    return; 
 }
+
+function writeInFile_main() {
+    let words = ["hola", "palabra", "si", "archivo", "clases", "array", "no", "JavaScript", "HTML", "CSS"];
+    let index_word = Math.floor(Math.random() * 10);
+
+    writeInFile(words[index_word]); 
+
+    return; 
+}
+
+writeInFile_main(); 
