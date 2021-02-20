3424dd77375bcc2bb0cb8fc3a5ea4abaf2f4d0f8
RafaelHinojosa <a01705777@itesm.mx>
2021-02-19 22:52:56 -0600

    Deportes y Musica added

--- lab8/webApp_html.js
+++ lab8/webApp_html.js
@@ -16,13 +16,70 @@ const server = http.createServer( (request, response) => {
     //MAIN
     response.write("<main>");
     
+    //Un poco sobre mi 
     response.write("<section>")
     response.write("<header><h2> Un poco sobre mi </h2></header>");
     response.write("<p> Hola! Soy Rafael Hinojosa Lopez y estudio el 4to semestre de Ingenieria en Tecnologias Computacionales.</p>");
     response.write("<p> En este sitio te compartire algunas de mis pasiones y hobbies. Mis datos de contacto los encontraras en la ultima seccion de la pagina.</p>");
     response.write("</section>");
     
+    //Aficiones
     response.write("<section>"); 
+    response.write("<h2>Aficiones</h2>"); 
+    
+    //Deportes
+    response.write("<article>"); 
+    response.write("<header><h3> Deportes </h3></header>");
+    response.write("<p>Soy fan de los deportes. Me gusta mucho saber como se juegan, quienes son los mejores equipos y ver eventos deportivos.</p>");
+    response.write("<p>La siguiente tabla contiene mis deportes, equipos y atletas favoritos.</p>"); 
+    
+    //Tabla de deportes
+    response.write("<table>"); 
+    
+    response.write("<thead>");
+    response.write("<tr>"); 
+    response.write("<th>Deporte</th>");
+    response.write("<th>Equipo favorito</th>");
+    response.write("<th>Jugador favorito</th>");
+    response.write("</tr>");
+    response.write("</thead>"); 
+
+    response.write("<tbody>");
+    response.write("<tr>"); 
+    response.write("<td>Beisbol</td>");
+    response.write("<td>New York Yankees</td>");
+    response.write("<td>Alex Rodriguez</td>");
+    response.write("</tr>");
+
+    response.write("<tr>"); 
+    response.write("<td>Futbol Soccer</td>");
+    response.write("<td>Tiburones Rojos</td>");
+    response.write("<td>Lionel Messi</td>");
+    response.write("</tr>");
+
+    response.write("<tr>"); 
+    response.write("<td>Golf</td>");
+    response.write("<td>Deporte Individual</td>");
+    response.write("<td>Tiger Woods</td>");
+    response.write("</tr>");
+    response.write("</tbody>"); 
+    
+    response.write("<tfoot></tfoot>"); 
+    response.write("</table>"); 
+    response.write("</article>"); 
+
+    //Música
+    response.write("<article>");
+    response.write("<header><h3>Musica</h3></header>"); 
+    response.write("<p>Me usta mucho hacer y disfrutar de la musica.</p>");
+    response.write("<p>Mis canciones favoritas son: </p>");
+    response.write("<ol>");
+    response.write("<li>Playtime - Yanni</li>");
+    response.write("<li>Tus Alas - Kim Richards</li>"); 
+    response.write("<li>Cara a cara - Majo y Dan, Marcos Vidal</li>"); 
+    response.write("</ol>"); 
+    response.write("</article>");
+
     response.write("</section>"); 
 
     response.write("</main>");
