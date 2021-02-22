d0f0276388491458c6ea733c9e07ee659ec3ccc5
RafaelHinojosa <a01705777@itesm.mx>
2021-02-22 12:51:34 -0600

    UTF-8 agregado, texto con acentos

--- lab8/ejemplo_servidor.js
+++ lab8/ejemplo_servidor.js
@@ -8,6 +8,7 @@ const server = http.createServer( (request, response) => {
     
     //Imprimir en http://localhost:PUERTO
     response.setHeader('Content-Type', 'text/html');    //necesario para dar FORMATO y entender HTML
+    response.write('<head><meta charset="UTF-8"></head>');
     response.write("Hola desde el servidor");     
     response.write("<h1>Hola desde el servidor</h1>");
     response.end(); 
--- lab8/webApp_html.js
+++ lab8/webApp_html.js
@@ -7,7 +7,7 @@ const server = http.createServer( (request, response) => {
     response.setHeader('Content-Type', 'text/html'); 
     
     //HEAD
-    response.write("<head><title> Rafa Hinojosa </title></head>");
+    response.write('<head><meta charset="UTF-8"><title> Rafa Hinojosa </title></head>');
 
     //BODY
     response.write("<body>"); 
@@ -20,8 +20,8 @@ const server = http.createServer( (request, response) => {
     response.write("<br>");
     response.write("<section>")
     response.write("<header><h2> Un poco sobre mi </h2></header>");
-    response.write("<p> Hola! Soy Rafael Hinojosa Lopez y estudio el 4to semestre de Ingenieria en Tecnologias Computacionales.</p>");
-    response.write("<p> En este sitio te compartire algunas de mis pasiones y hobbies. Mis datos de contacto los encontraras en la ultima seccion de la pagina.</p>");
+    response.write("<p> Hola! Soy Rafael Hinojosa López y estudio el 4to semestre de Ingeniería en Tecnologías Computacionales.</p>");
+    response.write("<p> En este sitio te compartiré algunas de mis pasiones y hobbies. Mis datos de contacto los encontrarás en la última sección de la página.</p>");
     response.write("</section>");
     
     //Aficiones
@@ -32,7 +32,7 @@ const server = http.createServer( (request, response) => {
     //Deportes
     response.write("<article>"); 
     response.write("<header><h3> Deportes </h3></header>");
-    response.write("<p>Soy fan de los deportes. Me gusta mucho saber como se juegan, quienes son los mejores equipos y ver eventos deportivos.</p>");
+    response.write("<p>Soy fan de los deportes. Me gusta mucho saber cómo se juegan, quiénes son los mejores equipos y ver eventos deportivos.</p>");
     response.write("<p>La siguiente tabla contiene mis deportes, equipos y atletas favoritos.</p>"); 
     
     //Tabla de deportes
@@ -48,13 +48,13 @@ const server = http.createServer( (request, response) => {
 
     response.write("<tbody>");
     response.write("<tr>"); 
-    response.write("<td>Beisbol</td>");
+    response.write("<td>Béisbol</td>");
     response.write("<td>New York Yankees</td>");
-    response.write("<td>Alex Rodriguez</td>");
+    response.write("<td>Alex Rodríguez</td>");
     response.write("</tr>");
 
     response.write("<tr>"); 
-    response.write("<td>Futbol Soccer</td>");
+    response.write("<td>Fútbol Soccer</td>");
     response.write("<td>Tiburones Rojos</td>");
     response.write("<td>Lionel Messi</td>");
     response.write("</tr>");
@@ -69,7 +69,7 @@ const server = http.createServer( (request, response) => {
     response.write("<tfoot></tfoot>"); 
     response.write("</table>"); 
 
-    response.write("<p>A quien prefieres?</p>");
+    response.write("<p>¿A quién prefieres?</p>");
     response.write("<form>"); 
     response.write("<input type='button' value='Messi'"); 
     response.write("<span>    </span>"); 
@@ -80,8 +80,8 @@ const server = http.createServer( (request, response) => {
     //Música
     response.write("<br>");
     response.write("<article>");
-    response.write("<header><h3>Musica</h3></header>"); 
-    response.write("<p>Me usta mucho hacer y disfrutar de la musica.</p>");
+    response.write("<header><h3>Música</h3></header>"); 
+    response.write("<p>Me gusta mucho hacer y disfrutar de la mússica.</p>");
     response.write("<p>Mis canciones favoritas son: </p>");
     response.write("<ol>");
     response.write("<li>Playtime - Yanni</li>");
@@ -89,9 +89,9 @@ const server = http.createServer( (request, response) => {
     response.write("<li>Cara a cara - Majo y Dan, Marcos Vidal</li>"); 
     response.write("</ol>"); 
     
-    response.write("<p>Con cual <strong>color</strong> identificas la musica?</p>");
+    response.write("<p>¿Con cuál <strong>color</strong> identificas la música?</p>");
     response.write("<form>");
-    response.write("<label>La musica es color...  ");
+    response.write("<label>La música es color...  ");
     response.write("<input type='color'/></label>"); 
     response.write("</form>"); 
     response.write("</article>");
@@ -101,8 +101,8 @@ const server = http.createServer( (request, response) => {
     response.write("<br><br>");
     response.write("<section>");
     response.write("<h2>Datos de Contacto</h2>"); 
-    response.write("<div>Nombre: Rafael Hinojosa Lopez</div>");
-    response.write("<div>Matricula: A01705777</div>");
+    response.write("<div>Nombre: Rafael Hinojosa López</div>");
+    response.write("<div>Matrícula: A01705777</div>");
     response.write("<div>Correo: a01705777@itesm.mx</div>");
     response.write("<div>Instagram: <a href='https://www.instagram.com/rafahinojosa.embajadorestec/'> @rafahinojosa.embajadorestec</a></div>");
     response.write("</section>");
@@ -114,7 +114,7 @@ const server = http.createServer( (request, response) => {
     response.write("<br><br>")
     response.write("<div>Laboratorio 8 de tc2005b</div>"); 
     response.write("<div>Editor HTML utilizado: Visual Studio Code</div>");
-    response.write("<div>Pagina web del editor: <a href='https://code.visualstudio.com/'> https://code.visualstudio.com </a></div>");
+    response.write("<div>Página web del editor: <a href='https://code.visualstudio.com/'> https://code.visualstudio.com </a></div>");
     response.write("</footer>");
 
     response.write("</body>");
