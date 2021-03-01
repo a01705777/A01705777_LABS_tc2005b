45da0e47be3310c8093bb2604b0b5630045367d5
RafaelHinojosa <a01705777@itesm.mx>
2021-03-01 16:11:43 -0600

    Basic Structure of Express@

--- lab11/app.js
+++ lab11/app.js
@@ -1,1 +1,20 @@
 console.log("Ya no tengo que andar reiniciando el servidor gracias a nodemon!!");
+
+//Basic Express Structure
+const express = require('express');
+const app = express();
+
+//Middleware
+app.use((request, response, next) => { 
+    console.log('Middleware!');
+    next();         //Vamos al siguiente use...
+});
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    response.send('Aqui se acaba con send');
+});
+
+app.listen(3000);
+
+
