5767254d438ac32060fc522d84b1f722f7994da7
RafaelHinojosa <a01705777@itesm.mx>
2021-03-01 17:00:34 -0600

    Roues with Express

--- lab11/app.js
+++ lab11/app.js
@@ -5,15 +5,31 @@ const express = require('express');
 const app = express();
 
 //Middleware
-app.use((request, response, next) => { 
-    console.log('Middleware!');
-    next();         //Vamos al siguiente use...
+app.use('/sobre_mi/deportes', (request, response, next) => {
+    console.log('Estas en deportes');
+    response.send('<h1> Messi the best </h1>');
 });
 
-app.use((request, response, next) => {
-    console.log('Otro middleware!');
-    response.send('Aqui se acaba con send');
+app.use('/sobre_mi/musica', (request, response, next) => {
+    console.log('Se va directo a sobre mi por el next');
+    next(); //Se va al siguiente que tenga el mismo patron minimo...  '/'
 });
+
+app.use('/sobre_mi', (request, response, next) => {
+    console.log('Estas en sobre mi');
+    response.send('Sobre mi');
+});
+
+app.use('/html', (request, response, next) => {
+    console.log('Estas en html');
+    response.send('<h1> Tarea HTML </h1>');
+});
+
+app.use('/', (request, response, next) => { 
+    console.log('Middleware!');
+    response.send('<h1> Middleware! </h1>');
+});
+
 
 app.listen(3000);
 
