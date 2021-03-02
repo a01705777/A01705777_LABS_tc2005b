1ea2a518464b63bed3e89f62a90ea37f831479fa
RafaelHinojosa <a01705777@itesm.mx>
2021-03-02 01:12:20 -0600

    Inicio Route added

--- lab11/app.js
+++ lab11/app.js
@@ -8,40 +8,14 @@ const app = express();
 //Routeador para utilizar el modulo de nombres, en la carpeta Routes
 const rutasNombres = require('./routes/nombres');
 const rutasTareas = require('./routes/tareas');
+const rutasInicio = require('./routes/inicio'); 
 
 //Middleware
-//Utilizar body Parser:
 app.use(bodyParser.urlencoded({ extended: false }));
 
-/*
-Pendiente...
-app.use('/sobre_mi/deportes', (request, response, next) => {
-    console.log('Estas en deportes');
-    response.send('<h1> Messi the best </h1>');
-});
-
-app.use('/sobre_mi/musica', (request, response, next) => {
-    console.log('Se va directo a sobre mi por el next');
-    next(); //Se va al siguiente que tenga el mismo patron minimo...  '/'
-});
-
-app.use('/sobre_mi', (request, response, next) => {
-    console.log('Estas en sobre mi');
-    response.send('Sobre mi');
-});
-*/
-
-
-/* Ejemplo de app.use con otro modulo
-app.use('/modulo', nombreRuta); */
-
+app.use('/', rutasInicio);
 app.use('/tareas', rutasTareas);
 app.use('/nombres', rutasNombres);
-
-app.get('/', (request, response, next) => {
-    console.log('Middleware!');
-    response.send('<h1> Middleware! </h1>');
-});
 
 app.use( (request, response, next) => {
     response.status(404).send('<h1> Page Not Found </h1>');
