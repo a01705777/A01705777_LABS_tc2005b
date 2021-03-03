acac38335f09db838105997cfd6a274e44b33d96
RafaelHinojosa <a01705777@itesm.mx>
2021-03-03 17:09:29 -0600

    Ruta / actualizada por HTML

--- lab12/app.js
+++ lab12/app.js
@@ -13,20 +13,19 @@ app.set('views', 'views');
 //Routers para acceder a la carpeta de rutas
 const rutasNombres = require('./routes/nombres');
 const rutasTareas = require('./routes/tareas');
-const rutasInicio = require('./routes/inicio'); 
 
 //Middleware
+
+//BodyParser
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //Acceder a los archivos de la carpeta Public
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Rutas a utilizar
-app.get('/personal', (request, response, next) => {
+app.get('/', (request, response, next) => {
     response.sendFile(path.join(__dirname, 'views', 'inicio.html'));
 })
-
-app.use('/', rutasInicio);
 app.use('/tareas', rutasTareas);
 app.use('/nombres', rutasNombres);
 app.use( (request, response, next) => {
