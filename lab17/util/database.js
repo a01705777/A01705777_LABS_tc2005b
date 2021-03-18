56df9450ec4c2e6fb0e2b666a7efaf477fb58576
a01705777 <a01705777@itesm.mx>
2021-03-17 21:30:03 -0600

    Database file created

--- lab17/util/database.js
+++ lab17/util/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'desarrollo_software',
+    database: 'labs_tc2005b',
     password: ''
     // port: por default: 3306
 });
