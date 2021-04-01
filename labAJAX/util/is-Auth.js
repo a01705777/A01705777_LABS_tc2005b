912962f3a81d68e65d995a48ad49895c4b65d96b
a01705777 <a01705777@itesm.mx>
2021-04-01 16:40:54 -0600

    404 Preguntas respondidas

--- labAJAX/util/is-Auth.js
+++ labAJAX/util/is-Auth.js
@@ -1,6 +1,6 @@
 module.exports = (request, response, next) => {
-    // if (!request.session.isLoggedIn) {
-    //     return response.redirect('/usuarios/login');
-    // }
+    if (!request.session.isLoggedIn) {
+        return response.redirect('/usuarios/login');
+    }
     next();
 }
